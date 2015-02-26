8e85a08d488dde6b52759f2d2c798370cd8f8463
Isaac Bruno <isaac.centenario@gmail.com>
2015-02-26 18:44:29 -0300

    Melhorias na classe

--- pagination.class.php
+++ pagination.class.php
@@ -1,6 +1,7 @@
 <?php 
 
-class pagination {
+class pagination 
+{
 
 	public $array;
 	public $page;
@@ -11,50 +12,56 @@ class pagination {
 	public $rt; 
 	public $start; 
 	public $end;
-	public function __construct( $array = null, $limit = null, $return_type = 'array' ) {
+	public $max;
+
+	public function __construct( $array, $limit, $return_type = 'array' ) 
+	{
 		$return_array = [];
-		if( is_object($array) ) { 
-			foreach( $array as $key=>$obj ) {
+		if( is_object($array) ) :
+
+			foreach( $array as $key=>$obj ):
 				$return_array[$key] = (array) $obj;
-			}
-		} else {
+			endforeach;
+
+		else:
 			$return_array = (array) $array; 
-		}
+		endif;
+
 		$this->array = (array)$return_array;
 		$this->limit = $limit;
 		$this->total = count( $this->array );
 		$this->pages = ceil( $this->total / $limit ); 
 		$this->rt = $return_type; 
-
 	}
 
-	public function page($page = 1) {
-
-		if( $page < 1 or $page > $this->pages ) {
+	public function page($page = 1) 
+	{
+		if( $page < 1 or $page > $this->pages ):
+			$this->page == false;
 			return false;
-		}
+		else:
+	
+			$this->page = $page;
+			$pages = $this->pages; 
+			$items = $this->total; 
 
-		empty( $page ) or null == $page ? $page = 1 : $page = $page; 
-
-		$this->page = $page;
-		$pages = $this->pages; 
-		$items = $this->total; 
-
-		$start = ( $page * $this->limit ) - $this->limit; 
-		$this->start = $start + 1;
-		$this->end = $this->limit + $this->start - 1; 
-		$paginated = array_slice( $this->array, $start, $this->limit );
-		$this->paginated = $paginated; 
-		$return_obj = []; 
-		if( $this->rt == 'array' ) :
-			return $paginated; 
-		elseif( $this->rt == 'object' ): 
-			foreach( $paginated as $value ) {
-				$return_obj[] = (object) $value; 
-			}
-			return (object) $return_obj; 
-		else: 
-			return $paginated; 
+			$start = ( $page * $this->limit ) - $this->limit; 
+			$this->start = $start + 1;
+			$this->end = $this->limit + $this->start - 1; 
+			$paginated = array_slice( $this->array, $start, $this->limit );
+			$this->paginated = $paginated; 
+			$return_obj = []; 
+			$this->max = count( $return_obj );
+			if( $this->rt == 'array' ) :
+				return (array)$paginated; 
+			elseif( $this->rt == 'object' ): 
+				foreach( $paginated as $value ) {
+					$return_obj[] = (object) $value; 
+				}
+				return (object) $return_obj; 
+			else: 
+				return $paginated; 
+			endif;
 		endif;
 	}
 	public function prev() {
